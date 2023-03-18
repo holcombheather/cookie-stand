ef6645d1130dacda691c2fc05c1c77f191c538bf
Heather Holcomb <holcombheather@gmail.com>
2023-03-18 16:56:35 -0700

    Debugged table rendering with correct header and footer after form creation

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// Lab 07 - Single Constructor Function and Tables
 let cookiedex = [];
 // let hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-// let table = document.getElementById('dataTable');
+// let table = document.getElementById('dataTable'); //moved to render
 let standOpen = 6;
 let standClose = 20;
 let hoursOpen = standClose - standOpen;
+let table = document.getElementById('dataTable');
 
 function Location(city, minCust, maxCust, avgPurchase) {
   this.city = city;
@@ -38,7 +38,7 @@ Location.prototype.generateCookiesPerHour = function() {
 
 
 Location.prototype.render = function() {
-  let table = document.getElementById('dataTable');
+  // let table = document.getElementById('dataTable'); //moved to be a global variable
   let tbody = document.createElement('tbody');
 
   table.appendChild(tbody);
@@ -65,7 +65,8 @@ Location.prototype.render = function() {
 
 
 function displayTableHeader(){
-  let table = document.getElementById('dataTable');
+  // let table = document.getElementById('dataTable');
+  // console.log("displayTableHeader");
   let thead = document.createElement('thead');
   let headerRow = document.createElement('tr');
 
@@ -77,17 +78,17 @@ function displayTableHeader(){
   headerRow.appendChild(blankTh);
 
   for (let i = standOpen; i <= standClose; i++){
-    let thead = document.createElement('th');
-
+    let headHours = document.createElement('th');
+    console.log(i);
     if (i === 12) {
-      thead.textContent = `${i}pm`;
+      headHours.textContent = `${i}pm`;
     } else if (i > 12) {
-      thead.textContent = `${i - 12}pm`;
+      headHours.textContent = `${i - 12}pm`;
     } else {
-      thead.textContent = `${i}am`;
+      headHours.textContent = `${i}am`;
     }
 
-    headerRow.appendChild(thead);
+    headerRow.appendChild(headHours);
   }
 
   let totalHeader = document.createElement('th');
@@ -98,20 +99,27 @@ function displayTableHeader(){
 function displayTableBody(){
   for (let i = 0; i < cookiedex.length; i++){
     let store = cookiedex[i];
-    store.generateCookiesPerHour();
+    // store.generateCookiesPerHour();
     store.render();
   }
 }
 
 function displayTableFooter(){
   console.log('tableFooter');
-  let table = document.getElementById('dataTable');
+  // let table = document.getElementById('dataTable');
+
 
   let tfoot = document.createElement('tfoot');
   let footerRow = document.createElement('tr');
 
+
   table.appendChild(tfoot);
   tfoot.appendChild(footerRow);
+
+  // let existingFooter = table.querySelector('tfoot');
+  // if (existingFooter) {
+  //   table.removeChild(existingFooter);
+  // }
 
   let totalFooter = document.createElement('td');
 
@@ -135,25 +143,55 @@ function displayTableFooter(){
     h++;
   }
 
+
   let grandTotalFooter = document.createElement('td');
   grandTotalFooter.textContent = grandTotal;
   footerRow.appendChild(grandTotalFooter);
 }
 
 
+function addNewLocation(event) {
+  event.preventDefault();
 
-let seattle = new Location('Seattle', 23, 65, 6.3);
-let tokyo = new Location('Tokyo', 3, 24, 1.2);
-let dubai = new Location ('Dubai', 11, 38, 3.7);
-let paris = new Location('Paris', 20, 38, 2.3);
-let lima = new Location('Lima', 2, 16, 4.6);
+  let city = event.target.city.value;
+  let minCust = parseInt(event.target.minCust.value);
+  let maxCust = parseInt(event.target.maxCust.value);
+  let avgPurchase = parseFloat(event.target.avgPurchase.value);
 
-
-window.onload = function() {
+  let newLocation = new Location(city, minCust, maxCust, avgPurchase);
+  newLocation.generateCookiesPerHour();
+  table.innerHTML = '';
   displayTableHeader();
   displayTableBody();
+
+  // Update the table footer with the new store information
   displayTableFooter();
-};
+
+  // Clear the form input fields
+  event.target.reset();
+}
+
+let form = document.getElementById('new-location-form');
+form.addEventListener('submit', addNewLocation);
+
+
+let seattle = new Location('Seattle', 23, 65, 6.3);
+seattle.generateCookiesPerHour();
+let tokyo = new Location('Tokyo', 3, 24, 1.2);
+tokyo.generateCookiesPerHour();
+let dubai = new Location ('Dubai', 11, 38, 3.7);
+dubai.generateCookiesPerHour();
+let paris = new Location('Paris', 20, 38, 2.3);
+paris.generateCookiesPerHour();
+let lima = new Location('Lima', 2, 16, 4.6);
+lima.generateCookiesPerHour();
+
+
+
+displayTableHeader();
+displayTableBody();
+displayTableFooter();
+
 
 
 
